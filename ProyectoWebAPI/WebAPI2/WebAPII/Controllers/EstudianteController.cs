0871c565e5efa8d1d82fb755d4b683d94fd775b8
kezh97 <kezh97@gmail.com>
2017-04-10 21:12:34 -0600

    Semana 14
    RESTful

---
+++ ProyectoWebAPI/WebAPI2/WebAPII/Controllers/EstudianteController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPII.Models;
+
+namespace WebAPII.Controllers
+{
+    public class EstudianteController : ApiController
+    {
+        // Para publicar se ocupa bin, Global.asax, Web.config
+        // Mover a Ineput --> wwwroot --> Crear carpeta
+        List<Estudiante> ListaEstudiantes = new List<Estudiante>()
+        {
+            new Estudiante {idEstudiante="123", nombreEstudiante="Kevin", apellido1Estudiante="Zheng",apellido2Estudiante="Wu" },
+            new Estudiante {idEstudiante="456", nombreEstudiante="Laura", apellido1Estudiante="Vargas", apellido2Estudiante="Castro" },
+            new Estudiante {idEstudiante="798", nombreEstudiante="Jose", apellido1Estudiante="Lopez", apellido2Estudiante="Porras" }
+        };
+
+        public IEnumerable<Estudiante> GetAllEstudiante()
+        {
+            return ListaEstudiantes;
+        }
+
+        public IHttpActionResult GetEstudiante(string id)
+        {
+            Estudiante objEstudiante = ListaEstudiantes.FirstOrDefault((x) => x.idEstudiante.Equals(id));
+
+            if (objEstudiante != null)
+            {
+                return Ok(objEstudiante);
+            }
+            else
+            {
+                return NotFound();
+            }
+           
+        }
+    }
+}
---
+++ ProyectoWebAPI/WebAPI2/WebAPII/Models/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPII.Models
+{
+    public class Estudiante
+    {
+        
+        public string idEstudiante { get; set; }
+        public string nombreEstudiante { get; set; }
+        public string apellido1Estudiante { get; set; }
+        public string apellido2Estudiante { get; set; }
+
+    }
+}
